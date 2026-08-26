e266474520b76a0ae16cb21dc64ad2cac25ea17c
Shekh Asif <shekhasifofficial2022@gmail.com>
2026-08-26 23:19:41 +0530

    version 1.0.3

--- db/migrations/0001_add_polishing_step2.sql
+++ db/migrations/0001_add_polishing_step2.sql
@@ -2,4 +2,5 @@
 -- weightIn2 and weightOut2 are nullable - they are only set when an item
 -- goes through a second polishing pass. Loss is cumulative (step1 + step2).
 ALTER TABLE `orders` ADD `weight_in_2` text;
+--> statement-breakpoint
 ALTER TABLE `orders` ADD `weight_out_2` text;
